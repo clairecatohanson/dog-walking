4b244d3c701b316290b7f8d32a11ae0452d70902
Claire Hanson <clairecatohanson@gmail.com>
2023-11-27 11:00:32 -0600

    refactor database and CityList to add cities array to allow for multiple walkers in a city

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -1,20 +1,45 @@
-import { getWalkers } from "./database.js"
+import { getWalkers, getCities } from "./database.js"
 
 const walkers = getWalkers()
-
+const cities = getCities()
 
 export const CityList = () => {
     let citiesHTML = "<ul>"
     
     document.addEventListener("click", (event) => {
-        const clickedCityEl = event.target
-        if (clickedCityEl.dataset.type === "city") {
-            window.alert(`${clickedCityEl.innerHTML} is being serviced by ${clickedCityEl.dataset.walkername}`)
+        const clickedEl = event.target
+        if (clickedEl.dataset.type === "city") {
+            const cityId = parseInt(clickedEl.dataset.id)
+            let walkerNames = []
+            for (const walker of walkers) {
+                if (cityId === walker.cityId) {
+                    walkerNames.push(walker.name)
+                }
+            }
+            let walkerString = ''
+            for (let i = 0; i < walkerNames.length; i++) {
+                walkerString += `${walkerNames[i]}, `
+            }
+            const f_walkerString = walkerString.slice(0, -2)
+            for (const city of cities) {
+                if (cityId === city.id) {
+                    window.alert(`${city.name} is being serviced by the following walkers:\n${f_walkerString}`)
+                }
+            }
         }
     })
 
-    for (const walker of walkers) {
-        citiesHTML += `<li data-type="city" data-walkername="${walker.name}">${walker.city}</li>`
+    // document.addEventListener("click", (event) => {
+    //     const clickedCityEl = event.target
+    //     if (clickedCityEl.dataset.type === "city") {
+    //         const cityId = parseInt(clickedCityEl.dataset.cityid)
+    //         for 
+    //         window.alert(`${clickedCityEl.innerHTML} is being serviced by ${clickedCityEl.dataset.walkername}`)
+    //     }
+    // })
+
+    for (const city of cities) {
+        citiesHTML += `<li data-type="city" data-id="${city.id}">${city.name}</li>`
     }
 
     citiesHTML += "</ul>"
